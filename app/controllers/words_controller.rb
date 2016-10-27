7448e7c53e29d5fd09c968cebde08e7b1ff0bb2c
Web Artist <nomi.sheikh@gmail.com>
2016-10-27 19:03:10 +0400

    Added editing words functionality.

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -8,14 +8,27 @@ get '/words/new' do
   erb :"/words/new"
 end
 
+	get '/words/:id/edit' do
+		@word = Word.find(params[:id])
+		erb :'/words/edit'		
+	end
+
 get '/words/:id' do
-  id = params[:id]
-  @word = Word.find_by_id(id)
+  @word = Word.find(params[:id])
   #@anagrams = Word.find_anagrams(@word)
   erb :"/words/show"
 end
 
+
+put '/words/:id' do
+		@word = Word.find(params[:id])
+		@word.text = params[:text]
+		@word.save
+		erb :"/words/show"
+	end
+	
 post '/words' do
-		@word = Word.create(text: params[:word])
+		@word = Word.create(text: params[:text])
    redirect "/words/#{@word.id}"
 end
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,38 @@
 module ApplicationHelper
+	def word_form_method(word)
+		if word.new_record?
+			@method = "post"
+		else
+			@method = "put"
+		end
+		return @method
+	end
+	
+	def word_form_action(word)
+		if word.new_record?
+			@action = "/words"
+		else
+			@action = "/words/#{word.id}"
+		end
+		return @action
+	end
+	
+	def word_form_class(word)
+		if word.new_record?
+			@class = "new-word"
+		else
+			@class = "edit-word"
+		end
+		return @class
+	end
+	
+	def word_form_id(word)
+		id =  params[:id]
+		if word.new_record?
+			@form_id = "new-word"
+		else
+			@form_id = "word-#{id}"
+		end
+		return @form_id
+	end	
 end
